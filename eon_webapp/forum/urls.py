5a9618cc339d95d08c93358916ddc4a05d61b0f6
Tanner <massahos@gmail.com>
2019-04-30 01:45:05 -0700

    Commenting things Part 1.

--- eon_webapp/forum/urls.py
+++ eon_webapp/forum/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
+# Setting up required urlpatterns
 urlpatterns = [
     path('index/', views.Index.as_view(), name='forum_index'),
     path('create/', views.CreateForum.as_view(), name='forum_create'),
@@ -8,6 +9,5 @@ urlpatterns = [
     path('view/<in_id>', views.ViewForum, name='forum_view'),
     path('view/thread/<thread_id>', views.ViewThread, name='thread_view'),
     # path('view/thread/<thread_id>/comment', views.CreateComment.as_view(), name='comment_create'),
-
     # path('view/<thread_id>', views.CreatePost.as_view(), name='thread_create'),
 ]
--- eon_webapp/forum/views.py
+++ eon_webapp/forum/views.py
@@ -1,3 +1,4 @@
+# Required import statements
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
@@ -10,21 +11,24 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.core.exceptions import ObjectDoesNotExist
 from django.http import HttpResponseRedirect
 
-
+# Displays the forums available to the user but only the ones that they
+# have access to
 class Index(generic.ListView):
     model = Forum
     template_name = 'forum/index.html'
     context_object_name = 'forums'
+    # Returns all the forums
     def get_queryset(self):
-        """Return all forums"""
         forums = Forum.objects.all()
         forums = [forum for forum in forums if (forum.group==None or self.validate_user_in_forum_group(forum))]
         return forums
+    # This function only displays the users groups he is on the forums
     def validate_user_in_forum_group(self,forum):
         if not self.request.user.is_authenticated:
             return False
         group_members = GroupMember.objects.filter(user=self.request.user)
         group_ids=[]
+        # Creates all group_ids which the user is in
         for group_member in group_members:
             group_ids += [group.id for group in group_member.group.all()]
         if forum.group.id in group_ids:
@@ -32,17 +36,20 @@ class Index(generic.ListView):
         else:
             return False
 
-# Class based so use mixin
+# Creation of CreateForum class that verifies if they are signed in
+# and displays a form to create a forum
 class CreateForum(LoginRequiredMixin,generic.CreateView):
     form_class = ForumCreationForm
     success_url = reverse_lazy('forum_index')
     template_name = 'forum/create_forum.html'
 
-# Class based so use mixin
+# Creation of CreatePost class that verifies if they are signed in
+# and displays a form to create a forum
 class CreatePost(LoginRequiredMixin,generic.CreateView):
     form_class = PostCreationForm
     template_name = 'forum/create_post.html'
 
+    # Returns essential information that can be used elsewhere
     def form_valid(self, form):
         form.instance.forum = Forum.objects.get(id=self.kwargs['forum_id'])
         form.instance.poster = self.request.user
@@ -62,21 +69,22 @@ def ViewForum(request,in_id):
         if not request.user.is_authenticated:
             group_member = None
         else:
+            # Checking members of certain groups and adding them to a list
             group_members = GroupMember.objects.filter(user = request.user)
             for group_member in group_members:
                 for group in group_member.group.all():
                     groups.append(group)
-        #     groups = None
     except ObjectDoesNotExist:
         group_member = None
-        # grab all Groups
+    # Grab all groups
     required_group = forum.group
+    # Check if we can display the forum
     if (required_group is not None and required_group in groups) or (required_group is None):
         return render(request, 'forum/view.html', {"forum_id": forum.id, "forum_name":forum.topic_name,"forum_desc":forum.description,"threads":threads})
     else:
         return HttpResponseRedirect(reverse_lazy('forum_index'))
 
-# Use decorators for permissions - no class based view
+# Function that creates the structure of how a user will view a thread of comments in a forum
 def ViewThread(request,thread_id):
     thread = Thread.objects.get(id=int(thread_id))
 
@@ -105,7 +113,6 @@ def ViewThread(request,thread_id):
                             else:
                                 new_entry.child_comment = new_thing.pk
                             print(new_entry, new_entry.child_comment, new_entry.parent_comment)
-                            # new_entry.save()
                         except:
                             pass
                     return redirect("thread_view", thread_id=thread.id)
